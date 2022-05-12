0ddf08c3f7d87f939117f0c037c341405b3dbc68
PH-Andre <102005677+PH-Andre@users.noreply.github.com>
2022-05-12 00:08:29 -0300

    Delete sis-bd2 directory

--- sis-bd2/README.md
+++
@@ -1,7 +0,0 @@
-# sis-bd2
-
-...\sis-bd2\public>
-
-php -S localhost:8080
-
-# mudei a regra de negocio para um livro poder ter mais de um autor 
--- sis-bd2/public/mysql.php
+++
@@ -1,3 +0,0 @@
-<?php
-mysqli_connect("localhost", "root", "20101997","db_app_db2") or die(mysqli_error());
-echo "Conectado ao MySQL Server!<br />";
